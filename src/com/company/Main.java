7e596ecc51003b8f559ad0cf8dd4a5939358a3e3
MaskedPaladin <carjien@gmail.com>
2022-03-22 10:09:11 +0100

    All functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
@@ -18,20 +19,53 @@ public class Main {
 	    // write your code here
         Menu menu = new Menu();
         int option = menu.mainMenu();
+        if (option == 1){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Name\nsurname1\nsurname2\nbirthdate\nactive");
+            String name = sc.next();
+            String surname1 = sc.next();
+            String surname2 = sc.next();
+            String birthdate = sc.next();
+            Boolean active = sc.nextBoolean();
+            PreparedStatement ps = con.prepareStatement("INSERT INTO player(name, surname1, surname2, birthdate, active) VALUES("+"'"+name+"',"+"'"+surname1+"',"+"'"+surname2+"',"+"TO_DATE("+"'"+birthdate+"'"+",'dd/mm/yyyy'"+")"+","+active+")");
+            ps.executeUpdate();
+        }
+        if (option == 2){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Name\nbowls_won");
+
+            String name = sc.next();
+            int bowls_won = sc.nextInt();
+
+            PreparedStatement ps = con.prepareStatement("INSERT INTO team(name, bowls_won) VALUES("+"'"+name+"'"+", "+bowls_won+")");
+            ps.executeUpdate();
+        }
         if (option == 3){
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM player");
+            PreparedStatement players = con.prepareStatement("SELECT * FROM player");
+            ResultSet rsplayers = players.executeQuery();
+            while (rsplayers.next()){
+                System.out.println("--------------------------------------------------------");
+                System.out.print("id_player: " + rsplayers.getInt("id_player"));
+                System.out.print("\nname: " + rsplayers.getString("name"));
+                System.out.print("\nsurname1: " + rsplayers.getString("surname1"));
+                System.out.print("\nsurname2: " + rsplayers.getString("surname2"));
+                System.out.print("\nbirthdate: " + rsplayers.getDate("birthdate"));
+                System.out.print("\nactive: " + rsplayers.getBoolean("active"));
+                System.out.println("\n--------------------------------------------------------");
+            }
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM team");
             ResultSet rs = ps.executeQuery();
             while (rs.next()){
                 System.out.println("--------------------------------------------------------");
-                System.out.print("id_player: " + rs.getInt("id_player"));
+                System.out.print("id_team: " + rs.getInt("id_team"));
                 System.out.print("\nname: " + rs.getString("name"));
-                System.out.print("\nsurname1: " + rs.getString("surname1"));
-                System.out.print("\nsurname2: " + rs.getString("surname2"));
-                System.out.print("\nbirthdate: " + rs.getDate("birthdate"));
-                System.out.print("\nactive: " + rs.getBoolean("active"));
+                System.out.println("\nbowls_won: " + rs.getInt("bowls_won"));
                 System.out.println("\n--------------------------------------------------------");
             }
-
+        }
+        if (option == 4){
+            PreparedStatement ps = con.prepareStatement("TRUNCATE TABLE player");
+            ps.executeQuery();
         }
         con.close();
     }
